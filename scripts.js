4c0494d45b3a71ce0983fcd249bdd3b0141b2d65
evillante <e.villante.work@gmail.com>
2014-11-18 11:58:15 +1100

    Selected image loading fixed
    Potential TODO, add loading gif

--- scripts.js
+++ scripts.js
@@ -3,10 +3,6 @@
 var windowWidth;	//window Dimensions
 var windowHeight;
 
-var $back;			//parallax layers
-var $mid;
-var $fore;
-
 var pageAdd =1;		//how many pages (of 5) have been displayed
 var potHeight;		//potential height of blog posts
 var randShape; 		//determines div clip
@@ -35,6 +31,7 @@ $(document).ready(function() {
 
 	potHeight = (windowHeight - 450 )/2;
 
+	//make parallax, trigger angular function when scrolled to the end
 	$(window).scroll(function() {	
 		scrollDist = $(window).scrollLeft();
 		docWidth = $(document).width();
@@ -46,7 +43,7 @@ $(document).ready(function() {
 		if( ((scrollDist + windowWidth)  >= (docWidth-3)) && blogsTaken < amountOfBlogs ) {
 			//call angular function
 			angular.element('#content-container').scope().getPost();	
-					
+			//add width to blog container					
 		   pageAdd++;
 		   $("#content").css('width' , 3000*pageAdd +'px');
 		}
@@ -55,63 +52,70 @@ $(document).ready(function() {
 });
 
 
-	app.controller('mainCtrl', function($scope, $scope, $http){
-		//api requirements
-		limit= 1;
-		consumerKey ="HEOkswS8esYqCMHCFlHqhhCa5ctvigQdwbEcz2fQD8rzPpE3bz";
-		account="visuallydefiant.tumblr.com"	
-			
-		//enlarge image for clicked blog
-		$scope.clickBlog = function(slot){
-			$scope.showSelected = true;
-			$http.jsonp('http://api.tumblr.com/v2/blog/'+ account + '/posts?callback=JSON_CALLBACK&api_key=' + consumerKey + '&limit=' + limit + '&offset=' + slot).success(function(postData) {
-				$scope.selectedBlogImage = postData.response.posts[0].photos[0].alt_sizes[0].url;
-			});
-		}		
-		//blog container
-		$scope.blogPostList = [];
+app.controller('mainCtrl', function($scope, $scope, $http){
+	//initially set the selected image to be a loading gif
+	var loadImg = "images/loading.png";
+	$scope.selectedBlogImage = loadImg;
+	//api requirements
+	limit= 1;
+	consumerKey ="HEOkswS8esYqCMHCFlHqhhCa5ctvigQdwbEcz2fQD8rzPpE3bz";
+	account="visuallydefiant.tumblr.com"	
+	//enlarge image when a blog is clicked
+	$scope.clickBlog = function(slot){
+		$scope.showSelected = true;
+		$http.jsonp('http://api.tumblr.com/v2/blog/'+ account + '/posts?callback=JSON_CALLBACK&api_key=' + consumerKey + '&limit=' + limit + '&offset=' + slot).success(function(postData) {
+			$scope.selectedBlogImage = postData.response.posts[0].photos[0].alt_sizes[0].url;
+		})
+	}		
+	//remove selected view and set the selected image to be a loading gif
+	$scope.clickOut = function(){
+		$scope.showSelected=false;
+		$scope.selectedBlogImage = loadImg;
+	}
 
-		//function to add blog posts
-		$scope.getPost = function(){
-			if(blogsTaken < block){
-				$http.jsonp('http://api.tumblr.com/v2/blog/'+ account + '/posts?callback=JSON_CALLBACK&api_key=' + consumerKey + '&limit=' + limit + '&offset=' + blogsTaken).success(function(postData) {
-			  		//find the number of posts in the blog
-			  		amountOfBlogs = postData.response.total_posts;
-			  		//store information to be displayes (per blog)
-			  		$scope.postImage = postData.response.posts[0].photos[0].alt_sizes[1].url;
-			  		$scope.postImage = postData.response.posts[0].photos[0].alt_sizes[1].url;		//get post image
-			  		$scope.postTitle = postData.response.posts[0].caption;							//get post title
-			  		$scope.blogData = postData.response;											//get post series id
-			  		$scope.randAng = "clip" + (1+ Math.floor(Math.random() * 9));					//give each post a random clip class
-			  		//add elements to blog array
-					$scope.blogPostList.push( {"image" : $scope.postImage, "inf" : $scope.postTitle, "postClip" : $scope.randAng, "postSlot" : blogsTaken} );
-					//get next blog post
-					blogsTaken++;
-					//run the function until 
-					$scope.getPost();				
-				});	
-			} else {
-				block += 5;
-			};	
-		};
-	})
+	//blog container
+	$scope.blogPostList = [];
+	//function to add blog posts
+	$scope.getPost = function(){
+		if(blogsTaken < block){
+			$http.jsonp('http://api.tumblr.com/v2/blog/'+ account + '/posts?callback=JSON_CALLBACK&api_key=' + consumerKey + '&limit=' + limit + '&offset=' + blogsTaken).success(function(postData) {
+		  		//find the number of posts in the blog
+		  		amountOfBlogs = postData.response.total_posts;
+		  		//store information to be displayes (per blog)
+		  		$scope.postImage = postData.response.posts[0].photos[0].alt_sizes[1].url;
+		  		$scope.postImage = postData.response.posts[0].photos[0].alt_sizes[1].url;		//get post image
+		  		$scope.postTitle = postData.response.posts[0].caption;							//get post title
+		  		$scope.blogData = postData.response;											//get post series id
+		  		$scope.randAng = "clip" + (1+ Math.floor(Math.random() * 9));					//give each post a random clip class
+		  		//add elements to blog array
+				$scope.blogPostList.push( {"image" : $scope.postImage, "inf" : $scope.postTitle, "postClip" : $scope.randAng, "postSlot" : blogsTaken} );
+				//get next blog post
+				blogsTaken++;
+				//run the function until 
+				$scope.getPost();				
+			});	
+		} else {
+			block += 5;
+		};	
+	};
+})
 
-	
-	//generate random height. feeds into ng-style
-	app.controller('blog', function($scope){
-		$scope.y = 80 + (Math.floor(Math.random() * potHeight));	
-		$scope.getHeight = function(){
-			return {'margin-top':$scope.y+'px'};
-		}
 
-	})
+//generate random height. feeds into ng-style
+app.controller('blog', function($scope){
+	$scope.y = 80 + (Math.floor(Math.random() * potHeight));	
+	$scope.getHeight = function(){
+		return {'margin-top':$scope.y+'px'};
+	}
 
-	 app.filter('stripText', function() {
-	    return function(text) {
-	      return String(text).replace(/<[^>]+>/gm, "").replace('&#8217;',"'");
-	    }
-	  }
-	);
+})
+//remove html tags and ascii
+app.filter('stripText', function() {
+    return function(text) {
+      return String(text).replace(/<[^>]+>/gm, "").replace('&#8217;',"'");
+    }
+  }
+);
 
 
 
